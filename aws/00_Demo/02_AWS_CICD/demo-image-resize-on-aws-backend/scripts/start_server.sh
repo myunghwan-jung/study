92cbcd670906df79e6e08d573eee5f38bc9be0bd
bonyj1012 <bonyj1012@gmail.com>
2025-05-12 19:47:59 +0900

    DB Started

--- aws/00_Demo/02_AWS_CICD/demo-image-resize-on-aws-backend/scripts/start_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Navigate to application directory
-cd /home/ec2-user/app || exit
-
-# Start the server using forever
-forever start -o out_resize.log -e err_resize.log src/server.js
-
-echo "Server started successfully."
